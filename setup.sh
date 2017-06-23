9cd80fbe90230f9253348fd1910f6b4eb97a3e8e
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2017-06-23 18:47:07 +0200

    Update setup.sh
    configuring github

--- setup.sh
+++ setup.sh
@@ -1,15 +1,29 @@
-local  GITHUB_NAME="carlosrodlop"
-local  GITHUB_EMAIL="it.carlosrodlop@gmail.com"
+export GITHUB_NAME="carlosrodlop"
+export GITHUB_EMAIL="it.carlosrodlop@gmail.com"
+export USER_VM="user"
+export USER_HOST="carlosrodlop"
+export SUDO_PASS="***"
+export IP_HOST=xxx.xxx.x.xxx
+export IP_VM=xxx.xxx.x.xxx
+
 
 ## As root
 
-sudo -i
+sudo -i sudo
 
 ## Folders
 
-mkdir code;mkdir code/github;mkdir code/github/$GITHUB_NAME 
-chown -R user:user code/
-chown -R user:user opt/
+mkdir code;mkdir code/github;mkdir code/github/$GITHUB_NAME
+mkdir ~/.ssh
+chown -R $USER_VM:$USER_VM code/
+chown -R $USER_VM:$USER_VM opt/
+
+## Copying files
+scp $USER_HOST@$IP_HOST:/Users/carlosrodlop/.ssh/myGitHubKey /home/user/.ssh
+
+## Adding SSH keys to SSH agent
+eval "$(ssh-agent -s)"
+ssh-add /home/user/.ssh/myGitHubKey
 
 ## Tools
 
@@ -19,7 +33,6 @@ apt-get update
 apt-get -y install git
 apt-get -y install oracle-java8-installer
 apt-get -y install maven
-
 
 git --version
 mvn -v
