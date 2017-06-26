d50ac487564310668e8589ee265936f2313b6a93
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2017-06-27 01:43:47 +0200

    Update setup.sh
    reformatting fuction

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ echo "IP_VM: $IP_VM"
 }
 
 setMyEnv(){
-mkdir code;mkdir code/github;mkdir code/github/$GITHUB_NAME;mkdir code/github/$GITHUB_ORG1
+mkdir code;mkdir code/github;mkdir code/github/$GITHUB_ORG1
 mkdir Support;mkdir Support/cases
 mkdir ~/.ssh
 mkdir ~/.m2
@@ -81,26 +81,28 @@ sudo docker version
 
 # https://github.com/moby/moby/issues/8791
 cgroupfs_mount() {
-# see also https://github.com/tianon/cgroupfs-mount/blob/master/cgroupfs-mount
-        if grep -v '^#' /etc/fstab | grep -q cgroup \
-                || [ ! -e /proc/cgroups ] \
-                || [ ! -d /sys/fs/cgroup ]; then
-                return
+  sudo -i
+  # see also https://github.com/tianon/cgroupfs-mount/blob/master/cgroupfs-mount
+  if grep -v '^#' /etc/fstab | grep -q cgroup \
+  || [ ! -e /proc/cgroups ] \
+  || [ ! -d /sys/fs/cgroup ]; then
+    return
+  fi
+  if ! mountpoint -q /sys/fs/cgroup; then
+    mount -t tmpfs -o uid=0,gid=0,mode=0755 cgroup /sys/fs/cgroup
+  fi
+  (
+    cd /sys/fs/cgroup
+    for sys in $(awk '!/^#/ { if ($4 == 1) print $1 }' /proc/cgroups); do
+      mkdir -p $sys
+      if ! mountpoint -q $sys; then
+        if ! mount -n -t cgroup -o $sys cgroup $sys; then
+          rmdir $sys || true
         fi
-        if ! mountpoint -q /sys/fs/cgroup; then
-                mount -t tmpfs -o uid=0,gid=0,mode=0755 cgroup /sys/fs/cgroup
-        fi
-        (
-                cd /sys/fs/cgroup
-                for sys in $(awk '!/^#/ { if ($4 == 1) print $1 }' /proc/cgroups); do
-                        mkdir -p $sys
-                        if ! mountpoint -q $sys; then
-                                if ! mount -n -t cgroup -o $sys cgroup $sys; then
-                                        rmdir $sys || true
-                                fi
-                        fi
-                done
-        )
+      fi
+    done
+  )
+  su $USER_VM
 }
 
 
