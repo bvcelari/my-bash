574f81bcd0ecffd4702b8da5d9e7abe54bf511d6
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2018-03-04 16:54:52 +0100

    update

--- ZSH/custom/bash_carlosrodlop.sh
+++ ZSH/custom/bash_carlosrodlop.sh
@@ -201,11 +201,13 @@ my-search-pattern-4file(){
    	if [ -f "$resultFile" ]; then
    		rm $resultFile
    	fi	
-   	echo "FILE"
    	echo "File : $file2search" >> $resultFile
-   	echo "\n\n TOTAL number of coincidences of $pattern" >> $resultFile
+   	echo "===================================" >> $resultFile
+   	echo "TOTAL number of coincidences of $pattern" >> $resultFile
+   	echo "\n\n===================================" >> $resultFile
    	cat $file2search | grep $pattern | sort | uniq -c >> $resultFile
-   	echo "\n\n Context of the $pattern" >> $resultFile
+   	echo "\n\n===================================" >> $resultFile
+   	echo "Context of the $pattern" >> $resultFile
    	cat $file2search | grep -A 6 -B 2 $pattern >> $resultFile
    	sublime $resultFile
    else
